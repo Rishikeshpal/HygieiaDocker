b8a8e04417a3ee70981a7917544faf702a6cba7f
Rishikeshpal <rishikesh.pal@gmail.com>
2019-11-02 17:33:12 +0000

    Create properties-builder.sh

---
+++ resources/jenkins/properties-builder.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "
+===========================================
+Properties file created `date`:  $PROP_FILE
+Note: passwords & apiKey hidden
+===========================================
+`cat $PROP_FILE |egrep -vi 'password|apiKey'`
+"
+
+exit 0
