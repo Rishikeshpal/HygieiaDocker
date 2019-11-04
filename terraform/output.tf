efe7ae948012233c41a359ddc824a182744a7c80
Rishikeshpal <rishikesh.pal@gmail.com>
2019-11-04 22:53:10 +0000

    Create output.tf

---
+++ terraform/output.tf
@@ -0,0 +1,13 @@
+output "HygieiadevSG_id" {
+  value = "${aws_security_group_HygieiadevSG.id}"
+}
+
+output "Hygieiadevamfam_public_ip" {
+  value = "${aws_instance.Hygieiadevamfam.public_ip}"
+}
+
+output "Hygieiadevamfam_public_dns" {
+  value = "${aws_instance.Hygieiadevamfam.public_dns}"
+}
+
+output "Hygieiadev_
